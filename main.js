fe1b3a1e98adfd62a2090e06238faf1e4b9b7177
Steven Dwy <yoplitein+git@gmail.com>
2022-02-11 12:43:30 -0800

    Implement tab functionality for separate editing of CSS and JS
    These have to be in their own models for suggestions to work; currently
    suggestions for inline CSS/JS require language server trickery that
    aren't feasible in standalone Monaco.
    See https://github.com/microsoft/monaco-editor/issues/2015#issuecomment-694715944

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-const monacoBaseURL = "https://cdn.jsdelivr.net/npm/monaco-editor@0.30.0/min/vs";
+const monacoBaseURL = "https://cdn.jsdelivr.net/npm/monaco-editor@0.32.1/min/vs";
 
 const skeletonTemplate = `
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- in case this gets copypasta'd -->
 	<meta charset="utf-8">
-	<style>
-	</style>
+	<title>untitled</title>
 </head>
 <body>
 	<h1>Hello, World!</h1>
-	
-	<!-- <script type="module">
-	</script> -->
 </body>
 </html>
 `.trim();
 const skeletonSelection = {
-	startLineNumber: 9,
-	endLineNumber: 10,
+	startLineNumber: 8,
+	endLineNumber: 8,
 	startColumn: 2,
 	endColumn: Infinity, // this works, but isn't documented
 };
@@ -51,21 +48,62 @@ function focusEditorEvent(ev)
 	editor.focus();
 }
 document.addEventListener("keydown", focusEditorEvent);
-// events dispatched within iframes don't bubble up, and the document is reset every refresh
-frame.addEventListener("load", () => {
-	frame.contentDocument.addEventListener("keydown", focusEditorEvent);
-});
 
+const resourceBlobURLs = [];
+function onFrameLoaded()
+{
+	// events dispatched within iframes don't bubble up, and the document is reset every refresh
+	frame.contentDocument.addEventListener("keydown", focusEditorEvent);
+	
+	resourceBlobURLs.forEach(url => URL.revokeObjectURL(url));
+	resourceBlobURLs.length = 0;
+}
+frame.addEventListener("load", onFrameLoaded);
+
+const models = {
+	html: null,
+	css: null,
+	js: null,
+};
+const domParser = new DOMParser();
+const xmlSerializer = new XMLSerializer();
 function refresh()
 {
-	//console.clear();
-	frame.srcdoc = editor.getModel().getValue();
+	const newDoc = domParser.parseFromString(models.html.getValue(), "text/html");
+	
+	const css = newDoc.createElement("link");
+	css.rel = "stylesheet";
+	css.href = URL.createObjectURL(new Blob([models.css.getValue()], { type: "text/css" }));
+	resourceBlobURLs.push(css.href);
+	newDoc.head.appendChild(css);
+	
+	const js = newDoc.createElement("script");
+	js.type = "module"; // TODO: option
+	js.src = URL.createObjectURL(new Blob([models.js.getValue()], { type: "text/javascript" }));
+	resourceBlobURLs.push(css.href);
+	newDoc.body.appendChild(js);
+	
+	//console.clear(); // TODO: option
+	frame.srcdoc = xmlSerializer.serializeToString(newDoc);
 }
 
+let currentModel = "html";
+const modelStates = {
+	html: null,
+	css: null,
+	js: null,
+};
 function switchTab(newTab)
 {
-	// TODO
-	console.log("switch to tab", newTab);
+	if(!(newTab in models))
+		throw new Error(`Unknown model ${newTab}`);
+	
+	modelStates[currentModel] = editor.saveViewState();
+	editor.setModel(models[newTab]);
+	currentModel = newTab;
+	
+	const state = modelStates[newTab];
+	if(state != null) editor.restoreViewState(state);
 }
 {
 	const tabs = document.querySelectorAll("#tabs .tab");
@@ -83,11 +121,14 @@ async function main()
 	require.config({ paths: { vs: monacoBaseURL } });
 	await new Promise(resolve => require(["vs/editor/editor.main"], resolve));
 	
+	models.html = monaco.editor.createModel(skeletonTemplate, "html");
+	models.css = monaco.editor.createModel("", "css");
+	models.js = monaco.editor.createModel("", "javascript");
+	
 	editor = monaco.editor.create(editorContainer, {
-		value: skeletonTemplate,
-		language: "html",
+		model: models.html,
 		theme: "vs-dark",
-		minimap: {enabled: false},
+		minimap: { enabled: false },
 		insertSpaces: false,
 		autoIndent: "full",
 	});
